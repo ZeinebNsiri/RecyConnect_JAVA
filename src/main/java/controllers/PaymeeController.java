ceaf1f93f1bccf018242c07fe81b642abeaada96
mnifsahar <saharmnif83@gmail.com>
2025-05-01 12:49:33 +0200

    iframe commit sahaar

--- src/main/java/controllers/PaymeeController.java
+++ src/main/java/controllers/PaymeeController.java
@@ -7,11 +7,11 @@ import javafx.scene.web.WebView;
 public class PaymeeController {
 
     @FXML
-    private WebView webView;
+    private WebView webview ;
 
     public void init(String paymentUrl) {
         // Load the URL into the WebView
-        WebEngine webEngine = webView.getEngine();  // Corrected to use the instance of webView
+        WebEngine webEngine = webview.getEngine();  // Corrected to use the instance of webView
         webEngine.load(paymentUrl);
     }
 }
